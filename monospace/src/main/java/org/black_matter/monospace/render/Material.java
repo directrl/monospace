d3322aaef5c6178b56af9062263bfc1fac4c2127
directrl <director@black-matter.org>
2024-10-05 11:56:03 +0200

    render/Material: add diffuse color

--- monospace/src/main/java/org/black_matter/monospace/render/Material.java
+++ monospace/src/main/java/org/black_matter/monospace/render/Material.java
@@ -3,6 +3,7 @@ package org.black_matter.monospace.render;
 import lombok.Getter;
 import lombok.Setter;
 import org.black_matter.monospace.util.Resource;
+import org.joml.Vector4f;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -11,11 +12,13 @@ import java.util.List;
 
 public class Material implements Closeable {
 	
-	@Getter @Setter private Texture texture;
 	@Getter private final List<Mesh> meshes;
+	@Getter @Setter private Texture texture;
+	@Getter @Setter private Vector4f diffuseColor;
 	
 	public Material() {
 		this.meshes = new ArrayList<>();
+		this.diffuseColor = new Vector4f(1, 1, 1, 1);
 	}
 	
 	public Material(Texture texture, List<Mesh> meshes) {
