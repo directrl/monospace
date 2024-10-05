30183e32f1ae636f672990db1de8b7cd5bba99c3
directrl <director@black-matter.org>
2024-10-05 17:33:51 +0200

    tools module

---
+++ monospace-tools/src/main/java/org/black_matter/monospace/tools/Main.java
@@ -0,0 +1,40 @@
+package org.black_matter.monospace.tools;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	
+	private static final List<String> AVAILABLE_TOOLS = new ArrayList<>();
+	
+	@Parameter(names={ "--tool", "-t" }) String tool = "";
+	
+	public static void main(String... args) {
+		Main main = new Main();
+		
+		JCommander.newBuilder()
+			.addObject(main)
+			.build()
+			.parse(args);
+		
+		main.run();
+	}
+	
+	public void run() {
+		if(!AVAILABLE_TOOLS.contains(tool)) {
+			System.out.println(String.format("Invalid tool: %s", tool));
+			System.out.println("Available tools:");
+			
+			AVAILABLE_TOOLS.forEach(System.out::println);
+			return;
+		}
+		
+		switch(tool) {
+			default:
+				throw new RuntimeException("Tool not implemented: " + tool);
+		}
+	}
+}
